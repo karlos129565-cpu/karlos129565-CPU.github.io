ad6ffeedb7fdef67c7bb4b25a67f372369d336fc
karlos129565-cpu <karlos129565@gmail.com>
2026-06-17 12:47:17 -0600

    marcas-data.js

--- marcas-data.js
+++ marcas-data.js
@@ -1,21 +1,21 @@
 const infoMarcas = {
   "GIORGIO ARMANI": {
-    img: "https://i.postimg.cc/SsCgyZyN/logo-gio.webp",
+    img: "./logo-gio.webp",
     descripcion: 'Giorgio Armani, nacido el 11 de julio de 1934 en Piacenza, Italia, es uno de los diseñadores de moda más influyentes y exitosos del mundo. Aunque inicialmente estudió medicina, abandonó esta carrera para dedicarse al diseño. Comenzó trabajando como escaparatista y luego como diseñador'
   },
   
   "ADIDAS": {
-    img: "https://i.postimg.cc/0jzn0Ty2/logo-adidas.webp",
+    img: "./logo-adidas.webp",
     descripcion: 'La historia de Adidas se remonta a la Gebrüder Dassler Schuhfabrik, creada en la década de 1920 por los hermanos Adolf y Rudolf Dassler en Herzogenaurach, Alemania. Inicialmente fabricaban zapatillas y calzado con clavos para deportistas, destacando por su innovación en el diseño y ajuste'
   },
   
   "AZZARO": {
-    img: "https://i.postimg.cc/RhG3qhBC/logo-azzaro.webp",
+    img: "./logo-azzaro.webp",
     descripcion: 'Azzaro, fundada por Loris Azzaro en 1967, ha forjado un camino distintivo en el mundo de las fragancias y la moda. La marca se originó en Túnez y se trasladó a París, donde Loris Azzaro comenzó a crear collares y complementos para su esposa, Michèle Carsy. Posteriormente, Azzaro se dedicó a la moda, lanzando su primera boutique en 1967 y su casa de alta costura en 1968.'
   },
   
   "CALVIN KLEIN": {
-    img: "https://i.postimg.cc/4ywKnyg3/logo-calvin.webp",
+    img: "./logo-calvin.webp",
     descripcion: 'Calvin Klein nació el 19 de noviembre de 1942 en el Bronx, Nueva York, en una familia judía de inmigrantes húngaros. Desde joven mostró interés por la moda y estudió en la Escuela Superior de Arte y Diseño y en el Instituto de Tecnología de la Moda de Nueva York (FIT), donde se graduó en Bellas Artes. Tras trabajar en varias empresas de ropa y abrigos, en 1968 fundó Calvin Klein, Inc. con un amigo de la infancia, iniciando con un préstamo de $10,000 dólares'
   }
 };
