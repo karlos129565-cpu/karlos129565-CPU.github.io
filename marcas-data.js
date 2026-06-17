90f6766061b020e14c883dcc3abe956ca7fe7302
karlos129565-cpu <karlos129565@gmail.com>
2026-06-17 11:44:35 -0600

    marcas-data.js

--- marcas-data.js
+++ marcas-data.js
@@ -2,5 +2,11 @@ const infoMarcas = {
 "GIORGIO ARMANI": {
 img: "https://i.postimg.cc/SsCgyZyN/logo-gio.webp",
 descripcion: "Giorgio Armani, nacido el 11 de julio de 1934 en Piacenza, Italia, es uno de los diseñadores de moda más influyentes y exitosos del mundo. Aunque inicialmente estudió medicina, abandonó esta carrera para dedicarse al diseño. Comenzó trabajando como escaparatista y luego como diseñador para la firma Nino Cerruti, donde perfeccionó su estilo. En 1975, fundó su propia marca, Giorgio Armani S.p.A., junto con su socio Sergio Galeotti. Armani revolucionó la moda con su enfoque minimalista y elegante, creando prendas como chaquetas holgadas y diseños que estilizan la figura. Su marca incluye líneas como Giorgio Armani, Emporio Armani y Armani Exchange, además de productos como perfumes, accesorios y mobiliario. Armani también ha tenido una estrecha relación con el cine, diseñando vestuarios para películas como American Gigolo. Su legado en la moda es reconocido mundialmente, y su estilo sigue siendo un símbolo de lujo y sofisticación"
+ },
+
+"ADIDAS": {
+img: "https://i.postimg.cc/0jzn0Ty2/logo-adidas.webp",
+descripcion: "La historia de Adidas se remonta a la Gebrüder Dassler Schuhfabrik, creada en la década de 1920 por los hermanos Adolf y Rudolf Dassler en Herzogenaurach, Alemania. Inicialmente fabricaban zapatillas y calzado con clavos para deportistas, destacando por su innovación en el diseño y ajuste del calzado. . Su gran oportunidad llegó en los Juegos Olímpicos de Berlín 1936, cuando persuadieron al velocista estadounidense Jesse Owens para usar sus zapatillas, logrando que Owens ganara cuatro medallas de oro y posicionando la marca a nivel internacional"
  }
 };
+
