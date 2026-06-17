38c5e63c7538409350e25a1e419abcf9232f8522
karlos129565-cpu <karlos129565@gmail.com>
2026-06-17 12:33:45 -0600

    marcas-data.js

--- marcas-data.js
+++ marcas-data.js
@@ -1,11 +1,11 @@
 const infoMarcas = {
   "GIORGIO ARMANI": {
-    img: "https://i.postimg.cc/SScGy2YW/logo-gio.webp",
+    img: "https://i.postimg.cc/SsCgyZyN/logo-gio.webp",
     descripcion: 'Giorgio Armani, nacido el 11 de julio de 1934 en Piacenza, Italia, es uno de los diseñadores de moda más influyentes y exitosos del mundo. Aunque inicialmente estudió medicina, abandonó esta carrera para dedicarse al diseño. Comenzó trabajando como escaparatista y luego como diseñador'
   },
   
   "ADIDAS": {
-    img: "https://i.postimg.cc/0jzn0TYz/logo-adidas.webp",
+    img: "https://i.postimg.cc/0jzn0Ty2/logo-adidas.webp",
     descripcion: 'La historia de Adidas se remonta a la Gebrüder Dassler Schuhfabrik, creada en la década de 1920 por los hermanos Adolf y Rudolf Dassler en Herzogenaurach, Alemania. Inicialmente fabricaban zapatillas y calzado con clavos para deportistas, destacando por su innovación en el diseño y ajuste'
   },
   
