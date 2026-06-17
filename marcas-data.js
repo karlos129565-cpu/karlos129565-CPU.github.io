c107793e2edf8367f9c3ea1b371b072a7a854f62
karlos129565-cpu <karlos129565@gmail.com>
2026-06-17 12:28:14 -0600

    marcas-data.js

--- marcas-data.js
+++ marcas-data.js
@@ -12,5 +12,10 @@ const infoMarcas = {
   "AZZARO": {
     img: "https://i.postimg.cc/RhG3qhBC/logo-azzaro.webp",
     descripcion: 'Azzaro, fundada por Loris Azzaro en 1967, ha forjado un camino distintivo en el mundo de las fragancias y la moda. La marca se originó en Túnez y se trasladó a París, donde Loris Azzaro comenzó a crear collares y complementos para su esposa, Michèle Carsy. Posteriormente, Azzaro se dedicó a la moda, lanzando su primera boutique en 1967 y su casa de alta costura en 1968.'
+  },
+  
+  "CALVIN KLEIN": {
+    img: "https://i.postimg.cc/4ywKnyg3/logo-calvin.webp",
+    descripcion: 'Calvin Klein nació el 19 de noviembre de 1942 en el Bronx, Nueva York, en una familia judía de inmigrantes húngaros. Desde joven mostró interés por la moda y estudió en la Escuela Superior de Arte y Diseño y en el Instituto de Tecnología de la Moda de Nueva York (FIT), donde se graduó en Bellas Artes. Tras trabajar en varias empresas de ropa y abrigos, en 1968 fundó Calvin Klein, Inc. con un amigo de la infancia, iniciando con un préstamo de $10,000 dólares'
   }
 };
