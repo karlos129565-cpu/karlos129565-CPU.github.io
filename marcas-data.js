23ac1649af39f39baa3823314d8286ad03bb6bb5
karlos129565-cpu <karlos129565@gmail.com>
2026-06-17 10:36:15 -0600

    marcas-data.js

--- marcas-data.js
+++ marcas-data.js
@@ -1,6 +1,6 @@
 const infoMarcas = {
 "GIORGIO ARMANI": {
-img: "https://i.postimg.cc/ZnJBT3XH/logo-acqua.webp",
+img: "https://i.postimg.cc/SsCgyZyN/logo-gio.webp",
 descripcion: "Giorgio Armani, nacido el 11 de julio de 1934 en Piacenza, Italia, es uno de los diseñadores de moda más influyentes y exitosos del mundo. Aunque inicialmente estudió medicina, abandonó esta carrera para dedicarse al diseño. Comenzó trabajando como escaparatista y luego como diseñador para la firma Nino Cerruti, donde perfeccionó su estilo. En 1975, fundó su propia marca, Giorgio Armani S.p.A., junto con su socio Sergio Galeotti. Armani revolucionó la moda con su enfoque minimalista y elegante, creando prendas como chaquetas holgadas y diseños que estilizan la figura. Su marca incluye líneas como Giorgio Armani, Emporio Armani y Armani Exchange, además de productos como perfumes, accesorios y mobiliario. Armani también ha tenido una estrecha relación con el cine, diseñando vestuarios para películas como American Gigolo. Su legado en la moda es reconocido mundialmente, y su estilo sigue siendo un símbolo de lujo y sofisticación"
  }
 };
